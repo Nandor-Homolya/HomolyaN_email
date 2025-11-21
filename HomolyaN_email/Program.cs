8375b203cf510b69b6678591fe6a07465249a722
homolyan <homolyan@kkszki.hu>
2025-11-21 11:06:14 +0100

    menürendszer

--- HomolyaN_email/Program.cs
+++ HomolyaN_email/Program.cs
@@ -10,6 +10,26 @@ namespace HomolyaN_email
     {
         static void Main(string[] args)
         {
+            while (true) { 
+                Console.Clear();
+                Console.WriteLine("0. Kilépés");
+                Console.WriteLine("1. emailek listázása");
+                Console.WriteLine("2. új email");
+                Console.WriteLine("3. email törlése");
+                Console.WriteLine("4. érvényes emailek listázása");
+                
+                string valasztas=Console.ReadLine();
+                switch (valasztas)
+                {
+                    case "0": return;
+                    case "1": Console.WriteLine("emailek listázása"); break;
+                    case "2": Console.WriteLine("új emailek");break;
+                    case "3": Console.WriteLine("email törlése"); break;
+                    case "4": Console.WriteLine("érvényes listázása");break;
+                    default : Console.WriteLine("rossz parancs");break;
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
